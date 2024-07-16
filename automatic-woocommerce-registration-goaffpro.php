f3c76b9e2599df24f077f484bf2bf2493f5d8326
Tariq Abdullah <tariq@ideaquotient.in>
2024-07-16 20:32:35 +0530

    Update automatic-woocommerce-registration-goaffpro.php

--- automatic-woocommerce-registration-goaffpro.php
+++ automatic-woocommerce-registration-goaffpro.php
@@ -20,7 +20,7 @@ function create_goaffpro_account($customer_id) {
     $email = $user->user_email;
     $first_name = get_user_meta($customer_id, 'billing_first_name', true);
     $last_name = get_user_meta($customer_id, 'billing_last_name', true);
-    $password = $_POST['password']; // Get the password from the registration form
+    $password = isset($_POST['password']) ? $_POST['password'] : wp_generate_password(); // Get the password from the registration form
     
     // Get GoAffPro API credentials from options
     $api_key = get_option('goaffpro_api_key');
@@ -86,6 +86,7 @@ function goaffpro_settings_init() {
     register_setting('goaffpro_settings', 'goaffpro_api_key');
     register_setting('goaffpro_settings', 'goaffpro_api_secret');
     register_setting('goaffpro_settings', 'goaffpro_show_refer_and_earn');
+    register_setting('goaffpro_settings', 'goaffpro_add_name_fields');
 
     add_settings_section(
         'goaffpro_settings_section',
@@ -117,6 +118,14 @@ function goaffpro_settings_init() {
         'goaffpro_settings',
         'goaffpro_settings_section'
     );
+
+    add_settings_field(
+        'goaffpro_add_name_fields',
+        __('Add Name Fields to Registration Form', 'goaffpro'),
+        'goaffpro_add_name_fields_render',
+        'goaffpro_settings',
+        'goaffpro_settings_section'
+    );
 }
 
 function goaffpro_api_key_render() {
@@ -134,6 +143,11 @@ function goaffpro_show_refer_and_earn_render() {
     echo '<input type="checkbox" name="goaffpro_show_refer_and_earn" ' . checked(1, $value, false) . ' value="1">';
 }
 
+function goaffpro_add_name_fields_render() {
+    $value = get_option('goaffpro_add_name_fields');
+    echo '<input type="checkbox" name="goaffpro_add_name_fields" ' . checked(1, $value, false) . ' value="1">';
+}
+
 function goaffpro_settings_section_callback() {
     echo __('Enter your GoAffPro API credentials here.', 'goaffpro');
 }
@@ -149,6 +163,61 @@ function goaffpro_options_page() {
         ?>
     </form>
     <?php
+}
+
+// Conditionally add first name and last name fields to the registration form
+$add_name_fields = get_option('goaffpro_add_name_fields');
+if ($add_name_fields) {
+    add_action('woocommerce_register_form_start', 'add_name_fields_to_registration_form');
+}
+
+function add_name_fields_to_registration_form() {
+    ?>
+    <p class="form-row form-row-first">
+        <label for="reg_billing_first_name"><?php _e('First Name', 'woocommerce'); ?> <span class="required">*</span></label>
+        <input type="text" class="input-text" name="billing_first_name" id="reg_billing_first_name" value="<?php if (!empty($_POST['billing_first_name'])) echo esc_attr($_POST['billing_first_name']); ?>" />
+    </p>
+    <p class="form-row form-row-last">
+        <label for="reg_billing_last_name"><?php _e('Last Name', 'woocommerce'); ?> <span class="required">*</span></label>
+        <input type="text" class="input-text" name="billing_last_name" id="reg_billing_last_name" value="<?php if (!empty($_POST['billing_last_name'])) echo esc_attr($_POST['billing_last_name']); ?>" />
+    </p>
+    <div class="clear"></div>
+    <?php
+}
+
+// Validate the fields
+add_action('woocommerce_register_post', 'validate_name_fields_on_registration', 10, 3);
+
+function validate_name_fields_on_registration($username, $email, $validation_errors) {
+    $add_name_fields = get_option('goaffpro_add_name_fields');
+    if ($add_name_fields) {
+        if (empty($_POST['billing_first_name'])) {
+            $validation_errors->add('billing_first_name_error', __('First name is required!', 'woocommerce'));
+        }
+
+        if (empty($_POST['billing_last_name'])) {
+            $validation_errors->add('billing_last_name_error', __('Last name is required!', 'woocommerce'));
+        }
+    }
+    return $validation_errors;
+}
+
+// Save the fields
+add_action('woocommerce_created_customer', 'save_name_fields_on_registration');
+
+function save_name_fields_on_registration($customer_id) {
+    $add_name_fields = get_option('goaffpro_add_name_fields');
+    if ($add_name_fields) {
+        if (isset($_POST['billing_first_name'])) {
+            update_user_meta($customer_id, 'billing_first_name', sanitize_text_field($_POST['billing_first_name']));
+            update_user_meta($customer_id, 'first_name', sanitize_text_field($_POST['billing_first_name']));
+        }
+
+        if (isset($_POST['billing_last_name'])) {
+            update_user_meta($customer_id, 'billing_last_name', sanitize_text_field($_POST['billing_last_name']));
+            update_user_meta($customer_id, 'last_name', sanitize_text_field($_POST['billing_last_name']));
+        }
+    }
 }
 
 // Add "Refer and Earn" tab to WooCommerce "My Account" page
